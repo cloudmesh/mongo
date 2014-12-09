c13cb8685dad70ed01cac4adfff8f022ec73babe
Gregor von Laszewski <laszewski@gmail.com>
2014-12-09 04:22:27 -0500

    introducing classes

--- bin/mongo-gregor.py
+++ bin/mongo-gregor.py
@@ -8,82 +8,99 @@ import yaml
 import json
 import sys
 
-
-class deploy_mongo(object):
+class known_hosts(object):
 
     def __init__(self):
-        self.username = cloudmesh.load().username()
-        with open('mongo-config.yaml', 'r') as f:
-            self.config = yaml.load(f)
-        self.data = self.config['config']
-        self.data["groupname"] = self.username + '-' + self.config['config']['group_postfix']
+        self.filename = os.path.expanduser('~/.ssh/known_hosts')
+        self.load()
         
+    def delete(self, prefix):
+        for key in self.keys:
+            if key.startswith(prefix):
+                ip = key.split()[0]
+                print "Deleting:",  ip
+
+    def load(self):
+        with open(self.filename, 'r') as f:
+            self.keys = f.read().split("\n")[:-1]
+
+    def save(self):
+        with open(self.filename, 'w') as f:
+            for key in self.keys:
+                f.write(key + "\n")
+
+                
+class deploy_mongo(object):
+
+    def __init__(self, filename='mongo-gregor.yaml'):
+        self.username = cloudmesh.load().username()
+        with open(filename, 'r') as f:
+            self.config = yaml.load(f)
+        pprint (self.config)
+        self.data = dict(self.config['config'].items() + self.config['cloud'].items())  
+        self.data["groupname"] = self.username + '-' + self.config['config']['group_postfix']
+
+    def _execute(self, commands):
+        for command in commands:
+            print "cm>", command
+            r = cloudmesh.shell(command)
+            print r
+        
+    def _prepare_cloudmesh(self):
+        commands = [
+            "cloud on {cloud}".format(**self.data),
+            "cloud select {cloud}".format(**self.data),
+            "project default {project}".format(**self.data)
+        ]
+        self._execute(commands)
+
+    def _delete_cluster(self):
+        commands = ['vm delete --cloud={cloud} --group={groupname} --force'.format(**self.data)]
+        self._execute(commands)
+                        
+    def _create_cluster(self):
+        commands = [str('cluster create '
+                      ' --force'
+                      ' --count={count}'
+                      ' --group={groupname}'
+                      ' --ln={login}'
+                      ' --cloud={cloud}'
+                      ' --flavor={flavor}'
+                      ' --image={image}'.format(**self.data))]
+        self._execute(commands)
+            
+
+#
+# initialize
+#
+
 service = deploy_mongo()
 
 print service.username
 pprint(service.config)
 pprint(service.data)
 
+#
+# prepare known hosts file
+#
+hosts = known_hosts()
+hosts.delete('149')
+hosts.save()
 
-# Hello World
-print ("Hello, World!")
+#
+# prepare cloudmesh
+#
+service._prepare_cloudmesh()
 
+#
+# remove old clusters
+#
+service._delete_cluster()
+service._create_cluster()
 
-
-sys.exit()
-
-# Reading yaml file for configuration
-with open('mongo-config.yaml', 'r') as f:
-    doc = yaml.load(f)
-
-cloud_name = doc["setup"]["cloud"]
-project = doc["setup"]["project"]
-group_name = doc["user"]["cluster_groupname"]
-login_name = doc["user"]["login_name"]
-flavor = doc["user"]["flavor"]
-count = doc["user"]["count"]
-image = doc["user"]["image"]
-
-data = {"groupname": group_name,
-        "cloud": cloud_name,
-        "project": project,
-        "login": login_name,
-        "flavor": flavor,
-        "count": count,
-        "image": image}
-pprint(data)
-
-r = cloudmesh.shell("cloud on {cloud}".format(**data))
-print r
-
-r = cloudmesh.shell("cloud select {cloud}".format(**data))
-print r
-
-r = cloudmesh.shell("project default {project}".format(**data))
-print r
-
-command = str('cluster create '
-              ' --force'
-              ' --count={count}'
-              ' --group={groupname}'
-              ' --ln={login}'
-              ' --cloud={cloud}'
-              ' --flavor={flavor}'
-              ' --image={image}'.format(**data))
-
-print command
 
 sys.exit()
 
-result = cloudmesh.shell('cluster create '
-                         ' --force'
-                         ' --count={count}'
-                         ' --group={groupname}'
-                         ' --ln={login}'
-                         ' --cloud={cloud}'
-                         ' --flavor={flavor}'
-                         ' --image={image}'.format(**data))
-pprint(result)
 
 # Display the list of all the VMs in the group
 json_data = cloudmesh.shell('group show {groupname} --format=json'.format(**data))
@@ -99,5 +116,4 @@ data = json.loads(json_data.replace('\r\n', '\\r\\n'))
 ip_list=str(data)
 pprint(ip_list)
 
-#Delete the group and the VMs in it
-cloudmesh.shell('vm delete --cloud={cloud} --group={groupname} --force'.format(**data))
+
