23ff8419091478ea55f65c86a1ddc1005b2576f1
Gregor von Laszewski <laszewski@gmail.com>
2014-12-09 02:28:51 -0500

    adding a multiline example

--- help/json-dec.py
+++ help/json-dec.py
@@ -1,5 +1,19 @@
 import json
 from pprint import pprint
+
 json_data= ('{"VM": ["rsinghania_175","rsinghania_174","rsinghania_176"]}')
+data = json.loads(json_data)
+pprint(data)
+
+
+txt = '''{
+    "VM": [
+        "rsinghania_175",
+        "rsinghania_174",
+        "rsinghania_176"
+        ]
+   }'''
+
+json_data= (txt)
 data = json.loads(json_data)
 pprint(data)
